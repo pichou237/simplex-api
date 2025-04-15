92f859988ccc6594ac572f396fcc2622f8b4a89d
josnel2 <josnelpamelfichieu@gmail.com>
2025-04-15 10:54:07 +0100

    resolution de conflit

--- manage_user/urls.py
+++ manage_user/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import UserRegisterView, VerifyEmailView, LoginUserView, UserDetailView, TechnicianDetailView, TechnicianUpdateView, TechnicianRegisterView
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import get_csrf_token
 
 
 urlpatterns = [
@@ -13,7 +12,6 @@ urlpatterns = [
     path('technicien/', TechnicianRegisterView.as_view(), name='technicien-list-create'),  
     path('technicien/<int:pk>/', TechnicianDetailView.as_view(), name='technicien-retrieve-update-destroy'), 
     path('technicien/update/', TechnicianUpdateView.as_view(), name='technicien-update'),
-    path('csrf/', get_csrf_token, name='csrf'),
 ]
    
 
