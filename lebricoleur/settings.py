4fa63a2424812e6a32b7ceba44025808cc18ae83
josnel2 <josnelpamelfichieu@gmail.com>
2025-04-22 11:33:35 +0100

    texte

--- lebricoleur/settings.py
+++ lebricoleur/settings.py
@@ -57,7 +57,6 @@ CSRF_TRUSTED_ORIGINS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ),
     
     'DEFAULT_THROTTLE_CLASSES': [
